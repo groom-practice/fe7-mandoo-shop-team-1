29cb18b4f37dc5f85903d619cbb4cce9c426b168
soundraven <kin9709@gmail.com>
2025-11-27 11:48:41 +0900

    세션스토리지에서 데이터 받아서 innerHTML에 이미지와 상품명, 가격 표시하는 기능 Order 버튼 누르면 확인 받고 주문내역 localStorage의 orders에 넣는 기능

---
+++ order/script.js
@@ -0,0 +1,73 @@
+const imgBase = "../imgs/"
+
+let orderedProduct = {}
+
+const renderProduct = () => {
+  const sessionData = sessionStorage.getItem("orderItem")
+  const orderedProductEl = document.querySelector(".order-product")
+
+  if (!sessionData) {
+    orderedProductEl.innerHTML = "<p>선택된 상품이 없습니다.</p>"
+    return
+  }
+
+  try {
+    orderedProduct = JSON.parse(sessionData)
+  } catch (error) {
+    console.error("Error parsing session data:", error)
+    orderedProductEl.innerHTML = "<p>선택된 상품이 없습니다.</p>"
+    return
+  }
+
+  orderedProductEl.innerHTML = `
+    <img src="${imgBase + orderedProduct.productImgFileName}" alt="${
+    orderedProduct.productName
+  }">
+  <div>
+    <p>${orderedProduct.productName}</p>
+    <p>${orderedProduct.productPrice}원</p>
+  </div>
+  `
+}
+
+const submitBtn = document.querySelector("#submitOrder")
+
+submitBtn.addEventListener("click", () => {
+  const name = document.querySelector("#orderName").value
+  const phone = document.querySelector("#orderPhoneNum").value
+  const address = document.querySelector("#orderAddress").value
+
+  if (!name || !phone || !address) {
+    alert("모든 정보를 입력해주세요")
+    return
+  }
+
+  const orderConfirm = confirm(
+    `name: ${name}\n\n` +
+      `phone: ${phone}\n\n` +
+      `address: ${address}\n\n` +
+      `위 정보로 주문하시겠습니까?`
+  )
+
+  if (!orderConfirm) {
+    alert("주문이 취소되었습니다.")
+    return
+  }
+
+  const order = {
+    product: orderedProduct,
+    name,
+    phone,
+    address,
+    date: new Date().toISOString(),
+  }
+
+  const existingOrders = JSON.parse(localStorage.getItem("orders")) || []
+
+  existingOrders.push(order)
+  localStorage.setItem("orders", JSON.stringify(existingOrders))
+  alert("주문이 완료되었습니다.")
+  window.location.href = "../index.html"
+})
+
+renderProduct()
